01433197d132890dab029779f4277c0bc956f0cc
pankajkrg21 <pankajkrg2016@gmail.com>
2024-11-23 21:02:20 +0530

    Create files.java

---
+++ models/files.java
@@ -0,0 +1,29 @@
+package models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "files")
+public class Files {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int fileId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String fileName;
+    private int fileSize;
+
+    @Lob
+    private byte[] encryptionKey;
+
+    private LocalDateTime uploadDate;
+    private boolean isEncrypted;
+
+    // Getters and Setters
+    // ...
+}
