bc81b08f830cf6946c8bf6832a73018db3275476
pankajkrg21 <pankajkrg2016@gmail.com>
2024-11-23 20:55:55 +0530

    Update user.java

--- models/user.java
+++ models/user.java
@@ -1,1 +1,32 @@
+package models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    private String username;
+    private String passwordHash;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLogin;
+
+    // Enum for roles
+    public enum Role {
+        ADMIN, USER
+    }
+
+    // Getters and Setters
+    // ...
+}
 
