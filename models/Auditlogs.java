1e33ef7526c9bbf2ff3860f1d906cdb7ea72f2a3
pankajkrg21 <pankajkrg2016@gmail.com>
2024-11-23 21:10:24 +0530

    Create Auditlogs.java

---
+++ models/Auditlogs.java
@@ -0,0 +1,34 @@
+package models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "audit_logs")
+public class AuditLogs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int auditId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "file_id")
+    private File file;
+
+    @Enumerated(EnumType.STRING)
+    private ActionType actionType;
+
+    private LocalDateTime actionTime;
+    private String description;
+
+    public enum ActionType {
+        ENCRYPT, DECRYPT
+    }
+
+    // Getters and Setters
+    // ...
+}
