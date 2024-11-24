cffc913a86ba47640b0b92e3c9f29bf7e16f8889
pankajkrg21 <pankajkrg2016@gmail.com>
2024-11-24 18:27:49 +0530

    Update UserDAO.java

--- models/dao/UserDAO.java
+++ models/dao/UserDAO.java
@@ -4,6 +4,8 @@ import models.User;
 import utils.DatabaseHelper;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDAO {
 
@@ -12,10 +14,10 @@ public class UserDAO {
         String query = "SELECT * FROM users WHERE username = ?";
         try (Connection connection = DatabaseHelper.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
-             
+
             statement.setString(1, username);
             ResultSet resultSet = statement.executeQuery();
-            
+
             if (resultSet.next()) {
                 return new User(
                     resultSet.getInt("id"),
@@ -28,5 +30,88 @@ public class UserDAO {
         return null; // User not found
     }
 
-    // Other CRUD methods (Create, Update, Delete) can be implemented similarly
+    // Create a new user
+    public boolean createUser(User user) throws SQLException {
+        String query = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
+        try (Connection connection = DatabaseHelper.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword()); // Consider hashing passwords before storing
+            statement.setString(3, user.getRole());
+
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
+        }
+    }
+
+    // Retrieve user by ID
+    public User getUserById(int id) throws SQLException {
+        String query = "SELECT * FROM users WHERE id = ?";
+        try (Connection connection = DatabaseHelper.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("username"),
+                    resultSet.getString("password"),
+                    resultSet.getString("role")
+                );
+            }
+        }
+        return null; // User not found
+    }
+
+    // Update an existing user
+    public boolean updateUser(User user) throws SQLException {
+        String query = "UPDATE users SET username = ?, password = ?, role = ? WHERE id = ?";
+        try (Connection connection = DatabaseHelper.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword()); // Hash passwords if necessary
+            statement.setString(3, user.getRole());
+            statement.setInt(4, user.getId());
+
+            int rowsUpdated = statement.executeUpdate();
+            return rowsUpdated > 0;
+        }
+    }
+
+    // Delete a user by ID
+    public boolean deleteUser(int id) throws SQLException {
+        String query = "DELETE FROM users WHERE id = ?";
+        try (Connection connection = DatabaseHelper.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, id);
+            int rowsDeleted = statement.executeUpdate();
+            return rowsDeleted > 0;
+        }
+    }
+
+    // Retrieve all users (Optional but useful)
+    public List<User> getAllUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String query = "SELECT * FROM users";
+        try (Connection connection = DatabaseHelper.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                User user = new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("username"),
+                    resultSet.getString("password"),
+                    resultSet.getString("role")
+                );
+                users.add(user);
+            }
+        }
+        return users;
+    }
 }
