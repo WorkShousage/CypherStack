f80c8d8ca54b36bf4ef6e974af008df41c5d080b
pankajkrg21 <pankajkrg2016@gmail.com>
2024-11-24 20:28:54 +0530

    Update File.java

--- models/File.java
+++ models/File.java
@@ -24,6 +24,75 @@ public class File {
     private LocalDateTime uploadDate;
     private boolean isEncrypted;
 
+    // Constructor to initialize the File object
+    public File(User user, String fileName, int fileSize, byte[] encryptionKey, boolean isEncrypted) {
+        this.user = user;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.encryptionKey = encryptionKey;
+        this.uploadDate = LocalDateTime.now();  // Automatically set the upload date to the current time
+        this.isEncrypted = isEncrypted;
+    }
+
+    // Method to create a File entry (you can use this in a service layer)
+    public static File createFile(User user, String fileName, int fileSize, byte[] encryptionKey, boolean isEncrypted) {
+        return new File(user, fileName, fileSize, encryptionKey, isEncrypted);
+    }
+
     // Getters and Setters
-    // ...
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public byte[] getEncryptionKey() {
+        return encryptionKey;
+    }
+
+    public void setEncryptionKey(byte[] encryptionKey) {
+        this.encryptionKey = encryptionKey;
+    }
+
+    public LocalDateTime getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(LocalDateTime uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public boolean isEncrypted() {
+        return isEncrypted;
+    }
+
+    public void setEncrypted(boolean encrypted) {
+        isEncrypted = encrypted;
+    }
 }
