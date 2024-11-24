43e121011d6e4b8dc28874ecb8de9365f2e035c1
pankajkrg21 <pankajkrg2016@gmail.com>
2024-11-24 20:27:07 +0530

    Update EncryptionKey.java

--- models/EncryptionKey.java
+++ models/EncryptionKey.java
@@ -26,6 +26,66 @@ public class EncryptionKey {
 
     private LocalDateTime createdAt;
 
+    // Constructor to initialize the EncryptionKey
+    public EncryptionKey(User user, byte[] publicKey, byte[] privateKey, byte[] aesKey) {
+        this.user = user;
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+        this.aesKey = aesKey;
+        this.createdAt = LocalDateTime.now();  // Automatically set the creation time to current time
+    }
+
+    // Method to create an EncryptionKey (you can use this from a service layer)
+    public static EncryptionKey createEncryptionKey(User user, byte[] publicKey, byte[] privateKey, byte[] aesKey) {
+        return new EncryptionKey(user, publicKey, privateKey, aesKey);
+    }
+
     // Getters and Setters
-    // ...
+    public int getKeyId() {
+        return keyId;
+    }
+
+    public void setKeyId(int keyId) {
+        this.keyId = keyId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public byte[] getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(byte[] publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public byte[] getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(byte[] privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public byte[] getAesKey() {
+        return aesKey;
+    }
+
+    public void setAesKey(byte[] aesKey) {
+        this.aesKey = aesKey;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
